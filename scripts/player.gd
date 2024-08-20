1bb295fe9c2751b11999da10443af28b1f5d1447
nerraf-dev <87069314+nerraf-dev@users.noreply.github.com>
2024-08-20 14:18:16 +0200

    chore: Update level.tscn and player.gd files

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 
 @export var gravity = 400.0
 @export var speed = 125.0
-const JUMP_VELOCITY = -400.0
+@export var jump_velocity = -200.0
 
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -13,10 +13,11 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+		if velocity.y > 500:
+			velocity.y = 500
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -25,5 +26,4 @@ func _physics_process(delta):
 		velocity.x = direction * speed
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-
 	move_and_slide()
